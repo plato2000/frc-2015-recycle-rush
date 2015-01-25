71b171ab06388dd24b239c6b570553568961d8c4
Parth Oza <plusparth@gmail.com>
2015-01-24 20:23:01 -0500

    Added slide drive
    Added slide drive, removed unused import, enabled toggling between
    drive modes

--- src/org/usfirst/frc/team4099/robot/drive/Driver.java
+++ src/org/usfirst/frc/team4099/robot/drive/Driver.java
@@ -3,12 +3,16 @@ package org.usfirst.frc.team4099.robot.drive;
 import org.usfirst.frc.team4099.control.Gamepad;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class Driver extends RobotDrive {
 	private DriveMode currentMode = DriveMode.ARCADE;
+	private SpeedController slider;
 	
-	public Driver(int frontLeftMotor, int rearLeftMotor, int frontRightMotor, int rearRightMotor) {
+	public Driver(int frontLeftMotor, int rearLeftMotor, int frontRightMotor, int rearRightMotor, int slider) {
 		super(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+		this.slider = new Talon(slider);
 	}
 	
 	public void setDriveMode(DriveMode type) {
@@ -31,6 +35,14 @@ public class Driver extends RobotDrive {
 		}
 	}
 	
+	public void omniWheelDrive(double forward, double side) {
+		this.slider.set(side);
+		this.m_frontLeftMotor.set(forward);
+		this.m_frontRightMotor.set(forward);
+		this.m_rearLeftMotor.set(forward);
+		this.m_rearRightMotor.set(forward);
+	}
+	
 	public void drive(Gamepad controller) {
 		switch (currentMode) {
 		case ARCADE:
@@ -38,8 +50,9 @@ public class Driver extends RobotDrive {
 			break;
 
 		case SLIDE:
-			//TODO: IMPLEMENT AN OMNIWHEEL (H/SLIDE DRIVE)
+			this.omniWheelDrive(controller.getLeftVerticalAxis(), controller.getLeftHorizontalAxis());
 			break;
 		}
 	}
+	
 }
