c48efce1a2150952412b4a2f3a223fcb4efea564
Parth Oza <plusparth@gmail.com>
2015-02-04 17:26:51 -0500

    Something that was left out

--- src/org/usfirst/frc/team4099/camera/RobotCamera.java
+++ src/org/usfirst/frc/team4099/camera/RobotCamera.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4099.camera;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 
 import org.usfirst.frc.team4099.control.Gamepad;
