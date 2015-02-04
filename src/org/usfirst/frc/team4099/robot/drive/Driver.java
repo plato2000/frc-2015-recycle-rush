737f8f3189be93d9825403bcac1f50f08f39b993
Parth Oza <plusparth@gmail.com>
2015-02-04 16:42:40 -0500

    Added autodrive class, changed direction to enum

--- src/org/usfirst/frc/team4099/robot/drive/Driver.java
+++ src/org/usfirst/frc/team4099/robot/drive/Driver.java
@@ -1,16 +1,16 @@
 package org.usfirst.frc.team4099.robot.drive;
 
+import org.usfirst.frc.team4099.camera.RobotCamera;
 import org.usfirst.frc.team4099.control.Gamepad;
-import org.usfirst.frc.team4099.robot.drive.SlideDriver;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Driver {
-	private DriveMode currentMode = DriveMode.SLIDE;
-    private RobotDrive arcadeDrive;
-    private SlideDriver slideDrive;
+	protected DriveMode currentMode = DriveMode.SLIDE;
+    protected RobotDrive arcadeDrive;
+    protected SlideDrive slideDrive;
 
-    public static final double REDUCTION_FACTOR = 2.0;
+    public static final double REDUCTION_FACTOR = 1.5;
 	public static final int FRONT_LEFT_MOTOR = 0;
 	public static final int REAR_LEFT_MOTOR = 1;
 	public static final int FRONT_RIGHT_MOTOR = 6;
@@ -20,11 +20,7 @@ public class Driver {
 	
 	public Driver() {
         arcadeDrive = new RobotDrive(FRONT_LEFT_MOTOR, REAR_LEFT_MOTOR, FRONT_RIGHT_MOTOR, REAR_RIGHT_MOTOR);
-<<<<<<< HEAD
-        slideDrive = new SlideDriver(arcadeDrive, SLIDE_MOTOR);
-=======
         slideDrive = new SlideDrive(arcadeDrive, FRONT_SLIDE_MOTOR, REAR_SLIDE_MOTOR);
->>>>>>> FETCH_HEAD
         arcadeDrive.setExpiration(0.1);
 	}
 	
@@ -48,10 +44,6 @@ public class Driver {
 		}
 	}
 
-    public void enterAutonomousMode() {
-        //TODO: Maybe implement something here?
-    }
-
     public void enterTeleoperatedMode() {
         this.arcadeDrive.setSafetyEnabled(true);
     }
@@ -68,29 +60,29 @@ public class Driver {
             // use left joystick to move forward and backward, left right to strafe
             // use left trigger button to turn left
             // use right trigger button to turn right
-            /*if (!(controller.isLeftTriggerPressed() && controller.isRightTriggerPressed())) {  // make sure that both turns are not pressed
-                if (controller.isLeftTriggerPressed())
-                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
-                                          controller.getLeftTrigger() / REDUCTION_FACTOR,
-                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);                    
-
-                else if (controller.isRightTriggerPressed())
-                    // note that the negative may be placed in the wrong if statement, test to see
-                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
-                                          controller.getRightTrigger() / REDUCTION_FACTOR,
-                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
-                else
-                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
-                                          0.0,
-                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
-            } else {
-                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
-                                          0.0,
-                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
-            }*/
-            slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
-                    controller.getLeftHorizontalAxis() / REDUCTION_FACTOR,
-                    controller.getRightHorizontalAxis() / REDUCTION_FACTOR);
+//            if (!(controller.isLeftTriggerPressed() && controller.isRightTriggerPressed())) {  // make sure that both turns are not pressed
+//                if (controller.isLeftTriggerPressed())
+//                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
+//                                          controller.getLeftTrigger() / REDUCTION_FACTOR,
+//                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
+//
+//                else if (controller.isRightTriggerPressed())
+//                    // note that the negative may be placed in the wrong if statement, test to see
+//                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
+//                                          -controller.getRightTrigger() / REDUCTION_FACTOR,
+//                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
+//                else
+//                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
+//                                          0.0,
+//                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
+//            } else {
+//                    slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR,
+//                                          0.0,
+//                                          controller.getLeftHorizontalAxis() / REDUCTION_FACTOR);
+//            }
+            System.out.println(controller.getRightHorizontalAxis());
+//			slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR, -controller.getLeftHorizontalAxis() / REDUCTION_FACTOR, controller.getRightHorizontalAxis());
+			slideDrive.slideDrive(controller.getLeftVerticalAxis() / REDUCTION_FACTOR, -controller.getRightHorizontalAxis() / REDUCTION_FACTOR, controller.getLeftHorizontalAxis());
 			break;
 		}
 	}
--- src/org/usfirst/frc/team4099/robot/drive/SlideDrive.java
+++ src/org/usfirst/frc/team4099/robot/drive/SlideDrive.java
@@ -1,25 +1,17 @@
 package org.usfirst.frc.team4099.robot.drive;
 
-import org.usfirst.frc.team4099.robot.Robot;
+//import org.usfirst.frc.team4099.robot.Robot;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.RobotDrive;
 
-<<<<<<< HEAD:src/org/usfirst/frc/team4099/robot/drive/SlideDriver.java
-public class SlideDriver {
-    private SpeedController sliderTalon;
-    private RobotDrive arcadeDrive;
-
-    public SlideDriver(RobotDrive arcadeDrive, int slider) {
-=======
 public class SlideDrive {
     private SpeedController sliderTalonFront;
     private SpeedController sliderTalonRear;
     private RobotDrive arcadeDrive;
 
     public SlideDrive(RobotDrive arcadeDrive, int sliderFront, int sliderRear) {
->>>>>>> FETCH_HEAD:src/org/usfirst/frc/team4099/robot/drive/SlideDrive.java
         this.arcadeDrive = arcadeDrive;
         this.sliderTalonFront = new Talon(sliderFront);
         this.sliderTalonRear = new Talon(sliderRear);
@@ -31,8 +23,19 @@ public class SlideDrive {
     }
 
     public void slideDrive(double forward, double turn, double strafe) {
-        Robot.debug.println("trying to move");
-        arcadeDrive.arcadeDrive(forward, turn);
-        this.slide(strafe);
+        if (Math.abs(forward) < 0.4) {
+        	forward = 0;
+        }
+        if (Math.abs(turn) < 0.5) {
+        	turn = 0;
+        }
+        
+    	arcadeDrive.arcadeDrive(forward, turn);
+        //Robot.debug.println("" + strafe);
+        if (Math.abs(strafe) > 0.3) {
+           this.slide(strafe);
+        } else {
+        	this.slide(0);
+        }
     }
 }
