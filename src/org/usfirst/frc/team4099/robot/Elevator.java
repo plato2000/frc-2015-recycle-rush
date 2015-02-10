f9b48dafac4fdbe11969626366b6e869dd35d4ea
Suriya Kandaswamy <sardines007@gmail.com>
2015-02-10 16:19:54 -0500

    hi

--- src/org/usfirst/frc/team4099/robot/Elevator.java
+++ src/org/usfirst/frc/team4099/robot/Elevator.java
@@ -12,31 +12,43 @@ public class Elevator {
 	public static final int UP_ENCODER_CHANNEL = 4;
 	public static final int DOWN_ENCODER_CHANNEL = 5;
 	
-	private Talon upElevatorTalon;
-	private Talon downElevatorTalon;
+	private Talon leftElevatorTalon;
+	private Talon rightElevatorTalon;
 	private Encoder encoder;
 	
 	public Elevator() {
-		this.upElevatorTalon = new Talon(LEFT_ELEVATOR);
-		this.downElevatorTalon = new Talon(RIGHT_ELEVATOR);
+		this.leftElevatorTalon = new Talon(LEFT_ELEVATOR);
+		this.rightElevatorTalon = new Talon(RIGHT_ELEVATOR);
 		this.encoder = new Encoder(UP_ENCODER_CHANNEL, DOWN_ENCODER_CHANNEL);
 		this.encoder.reset();
 	}
 	
 	public void move(Gamepad control) {
 		if (control.isDPadUpPressed()) {
-			this.upElevatorTalon.set(0.4);
-		} 
+			this.leftElevatorTalon.set(0.4);
+			this.rightElevatorTalon.set(0.4);
+		} else {
+			if (this.encoder.getDistance() > 0) {
+				double travelled = this.encoder.getDistance();
+				this.encoder.reset();
+				while (this.encoder.getDistance()<=travelled) {
+					this.leftElevatorTalon.set(-0.2);
+					this.rightElevatorTalon.set(-0.2);
+				}
+				this.leftElevatorTalon.set(0);
+				this.rightElevatorTalon.set(0);
+			}
+		}
 		/*
 		if (control.isBButtonPressed()) {
-			this.upEncoder.set(0.4);
-			this.downEncoder.set(0.4);
+			this.leftEncoder.set(0.4);
+			this.rightEncoder.set(0.4);
 		} else if (control.isAButtonPressed()) {
-			this.upEncoder.set(-0.225);
-			this.downEncoder.set(-0.225);
+			this.leftEncoder.set(-0.225);
+			this.rightEncoder.set(-0.225);
 		} else {
-			this.upEncoder.set(0.4);
-			this.downEncoder.set(0.4);
+			this.leftEncoder.set(0.4);
+			this.rightEncoder.set(0.4);
 		}
 		*/
 	}
